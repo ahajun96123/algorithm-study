4d3460b0ec6d2394b43a8fb72f5d897c6e6bc117
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-03-07 20:12:22 +0900

    [Bronze II] Title: 숫자의 개수, Time: 72 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -4,12 +4,10 @@ m = 1
 for _ in range(3):
   m *= int(sys.stdin.readline().rstrip())
 m = str(m)
-l = {}
-for i in range(10):
-  l[i] = 0
+l = [0 for _ in range(10)]
 
 for n in m:
   l[int(n)] += 1
 
-for i in range(10):
-  print(l[i])
+for i in l:
+  print(i)

b939891421ffadebe544ff576c0690d468c49adc
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-03-17 23:21:21 +0900

    Create 1193번.py
    크롬 '백준허브' 확장프로그램 오류로 인한 직접 커밋

---
+++ 백준/백준허브오류/1193번.py
@@ -0,0 +1,13 @@
+n = int(input())
+x = 1
+while x * (x + 1) / 2 < n:
+  x += 1
+h = x*(x-1)//2
+b = n - h
+p = ''
+if x % 2 == 1:
+  p = f'{x-b+1}/{b}'
+else: p = f'{b}/{x-b+1}'
+print(p)
+
+'''https://www.acmicpc.net/problem/1193'''

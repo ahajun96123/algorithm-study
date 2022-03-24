4e1c71e116fe63681ac25c806d602fe84bf415c1
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-03-24 15:14:36 +0900

    [Silver V] Title: 소인수분해, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.py
+++ 백준/Silver/11653. 소인수분해/소인수분해.py
@@ -1,8 +1,6 @@
 n = int(input())
-l = []
-for i in range(2, n+1):
+for i in range(2, int(n**0.5+1)):
   while n%i == 0:
     n //= i
-    l.append(i)
-
-print(*l, sep='\n')
+    print(i)
+if n != 1: print(n)

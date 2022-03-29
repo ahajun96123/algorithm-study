eeaf36422b28e13dacc2c3cccfa501342a806f0b
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-03-29 21:09:48 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 536 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,13 +1,12 @@
-sosu = [0 for i in range(10001)]
-sosu[1] = 1
-for i in range(2, 98):
-  for j in range(i * 2, 10001, i):
-    sosu[j] = 1
-t = int(input())
-for i in range(t):
-  a = int(input())
-  b = a // 2
-  for j in range(b, 1, -1):
-    if sosu[a - j] == 0 and sosu[j] == 0:
-      print(j, a - j)
+l = [1]*10000
+l[1] = 0
+for i in range(2, 100):
+  for j in range(i*2, 10000, i):
+    l[j] = 0
+for _ in range(int(input())):
+  n = int(input())
+  for i in range(n//2, 1, -1):
+    if l[i] == 1 and l[n-i] == 1:
+      print(i, n-i)
       break
+      

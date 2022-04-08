cc8e7cd6535ce99e0fbab9f2c5b868b81ab8542e
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-04-08 18:56:53 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,31 +1,10 @@
 n, m = map(int, input().split())
 l = list(map(int, input().split()))
-from itertools import combinations
+from itertools import combinations as com
 
-# a = 0
-
-# maxim = 0
-
-# for i in range(n):
-#   if l[i]+a< m:
-#     last = i
-#     a += l[i]
-#   if maxim < a: maxim = a
-
-#   if maxim==m: break
-
-# print(maxim)
 maxim = 0
-try:
-  c = combinations(l, 3)
-  t = list(map(sum, c))
-  aa = [_ for _ in t]
-  # print(*aa)
-  for su in t:
-    if su<=m and su>maxim: maxim=su
-    # print('max: ',maxim)
-    if maxim==m: raise Exception
-except:
-  pass
+t = map(sum, com(l, 3))
+for s in t:
+  if s<=m and s>maxim: maxim=s
+  if maxim==m: break
 print(maxim)
-

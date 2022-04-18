0e06a0e5791b68f35a7381e4d091c3cf88dcbb19
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-04-18 19:25:53 +0900

    [Silver III] Title: 통계학, Time: 420 ms, Memory: 52628 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -2,16 +2,10 @@ import sys
 from collections import Counter as c
 input = sys.stdin.readline
 n = int(input())
-l = []
-for _ in range(n):
-  l.append(int(input()))
-
+l = [int(input()) for _ in range(n)]
 l.sort()
 print(round(sum(l)/n))
 print(l[n//2])
-
 f = c(l).most_common(2)
-
 print(f[0][0] if len(f)==1 else f[0][0] if f[0][1]>f[1][1] else f[1][0])
-
 print(l[-1] - l[0])

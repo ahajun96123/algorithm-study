1de4bf7621a11f070cec46737e1b7019891e8688
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-04-23 00:43:01 +0900

    [Silver V] Title: 나이순 정렬, Time: 268 ms, Memory: 47276 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,5 +1,5 @@
 import sys
 input = sys.stdin.readline
-l = map(lambda x: [int(x[0]), x[1]],[input().split()for _ in range(int(input()))])
+l = [(lambda x,y: [int(x),y])(*input().split())for _ in range(int(input()))]
 for i in sorted(l, key=lambda x: x[0]):
   print(*i)

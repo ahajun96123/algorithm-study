cf0d7cf0574b4c39ea87be7718642a014f8eb85e
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-04-27 01:12:19 +0900

    [Silver V] Title: 최대공약수와 최소공배수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,3 +1,6 @@
-import math as m
 a, b = map(int, input().split())
-print(d:=m.gcd(a, b), a*b//d)
+t1, t2 = (a, b) if a>b else (b, a)
+while t1 % t2:
+  t1 %= t2
+  t1, t2 = t2, t1
+print(t2, a*b//t2)

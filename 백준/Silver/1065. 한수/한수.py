85218a1b6b46d2973a33c67bb7fbb331d5eb1e09
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-06-14 07:00:58 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,10 +1,9 @@
 n = int(input())
-se = {i for i in range(1, 100)}
-for i in range(1, 10):
-  for d in range(i, 10):
-    d1 = (d-i)//2
-    se.add(100*i + 10*(i+d1) + i+2*d1)
-  for d in range(i+1):
-    d2 = d//2
-    se.add(100*i + 10*(i-d2) + i-2*d2)
-print(len(set(filter(lambda x: x<=n, se))))
+cnt = 0
+for i in range(1, n+1):
+  if i<100: cnt+=1; continue
+  s = str(i)
+  for j in range(len(s)-2):
+    if not int(s[j]) - int(s[j+1]) == int(s[j+1]) - int(s[j+2]): break
+  else: cnt+=1
+print(cnt)

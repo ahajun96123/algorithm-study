2d490948fb777649a683de696c952dd7d6724bca
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-06-13 23:07:36 +0900

    [Silver I] Title: 회의실 배정, Time: 332 ms, Memory: 57760 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+l = [list(map(int, input().split())) for _ in range(int(input()))]
+l.sort(key=lambda x: (x[1], x[0]))
+cnt, line = 0, 0
+for start, end in l:
+  if start >= line:
+    line = end
+    cnt += 1
+print(cnt)

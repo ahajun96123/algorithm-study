bf3f326ea6712b5af02c5a076af79247fb0f29e0
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-06-15 13:52:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,10 +1,9 @@
 n = int(input())
 INF = int(1e9)
 d = [INF]*(n+1)
-d[0] = 0
-if n>=3:
-  d[3]=1
-  if n>=5: d[5] = 1
+d[0] =0
+if n>=5: d[5] = 1
+if n>=3: d[3] = 1
 for i in range(6, n+1):
-  d[i] = min(d[i-3], d[i-5]) +1
-print(ans if (ans:=d[n]) <INF else -1)
+  d[i] = min(d[i-3], d[i-5]) + 1
+print(-1 if d[n] >= INF else d[n])

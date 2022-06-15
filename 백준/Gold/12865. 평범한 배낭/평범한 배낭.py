0c1cfeb49d6aa138ef94a9b61d224dc7c23e5220
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-06-15 21:53:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 3616 ms, Memory: 33668 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -4,8 +4,6 @@ n, k = inp()
 bag = [0]*(k+1)
 for _ in range(n):
   w, v = inp()
-  bag_t = bag[:]
-  for cw in range(k+1):
-    if cw >= w:
-      bag[cw] = max(bag_t[cw], bag_t[cw-w] + v)
+  for cw in range(k, w-1, -1):
+    bag[cw] = max(bag[cw], bag[cw-w] + v)
 print(bag[k])

b3025e486246ec9aa40950c8eeda99e528bbe9c6
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-06-15 18:10:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,12 +1,9 @@
 import sys
 inp = lambda: int(sys.stdin.readline())
 n = inp()
-l = [inp() for _ in range(n)]
-m = max(l)
-d = [0]*(m+1)
-if n>=3: d[3] = 4
-if n>=2: d[2] = 2
-if n>=1: d[1] = 1
-for i in range(4, m+1):
-  d[i] = d[i-1]+d[i-2]+d[i-3]
-for i in l: print(d[i])
+d = [0]*(11)
+d[3], d[2], d[1] = 4, 2, 1
+for i in range(4, 11):
+  d[i] = d[i-1] + d[i-2] + d[i-3]
+for _ in range(n):
+  print(d[inp()])

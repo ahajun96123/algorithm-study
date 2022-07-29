14cc4f0972f8ba853eb6e880a5c480c798cabcd9
JunmO Lee <39722430+ahajun96123@users.noreply.github.com>
2022-07-30 07:23:18 +0900

    [Silver II] Title: 팩토리얼5, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1564. 팩토리얼5/팩토리얼5.c
+++ 백준/Silver/1564. 팩토리얼5/팩토리얼5.c
@@ -6,24 +6,17 @@ int main(){
     long long int N;
     scanf("%d",&N);
 
-    long long int num=1;
+    long long int num = 1;
 
-        for(long long int i=1; i<=N; i++){
+    for(long long int i=1; i<=N; i++){
         num=num*i;
-        while(num%10==0) num=num/10;
+        while(num % 10 == 0) num /= 10;
 
-        //if((i+1)/1000000==0) num=num%100000000000;
-        //else if((i+1)/100000==0) num=num%10000000000;
-        //else if((i+1)/10000==0) num=num%1000000000;
-        //else if((i+1)/1000==0) num=num%100000000;
-        //else if((i+1)/100==0) num=num%10000000;
-        //else if((i+1)/10==0) num=num%1000000;
-        //else num=num%1000000000;
-        num=num%1000000000000;
+        num %= 1000000000000;
     }
 
-    num=num%100000;
-
+    num %= 100000;
+  
     if(num/10000==0) printf("%d",0);
     if(num/1000==0) printf("%d",0);
     if(num/100==0) printf("%d",0);
